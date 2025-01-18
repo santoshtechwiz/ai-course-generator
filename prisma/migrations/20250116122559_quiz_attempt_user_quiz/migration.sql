dc35850527e2b37006687e5f6fd43b9e0a598980
santosh <sansi@orsted.com>
2025-01-18 22:08:35 +0800

    init

--- prisma/migrations/20250116122559_quiz_attempt_user_quiz/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,quizId]` on the table `QuizAttempt` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "QuizAttempt_userId_quizId_key" ON "QuizAttempt"("userId", "quizId");
