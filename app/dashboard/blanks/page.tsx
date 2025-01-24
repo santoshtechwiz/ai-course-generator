96c94018e8d5771eeca11a464cac01bcdbbb7c07
santosh <santosh.ksingh03@gmail.com>
2025-01-24 09:33:41 +0800

    suspense

--- app/dashboard/blanks/page.tsx
+++ app/dashboard/blanks/page.tsx
@@ -1,10 +1,10 @@
+import { Suspense } from "react";
 import { AnimatedQuizHighlight } from "@/app/components/AnimatedQuizHighlight";
 import FillInTheBlankQuizForm from "../components/BlankQuizForm";
 import { getAuthSession } from "@/lib/authOptions";
 import { QuizWrapper } from "@/components/QuizWrapper";
 
 export default async function QuizPage() {
-  const session = await getAuthSession();
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-8 text-center">
@@ -13,13 +13,15 @@ export default async function QuizPage() {
       <div className="flex flex-wrap md:flex-nowrap gap-8">
         {/* Left Column - Fill in the Blank Form */}
         <div className="flex-grow bg-white dark:bg-gray-800 p-8 shadow rounded">
-         <QuizWrapper type={"fill-in-the-blanks"}>
-         
-         </QuizWrapper>
+          <Suspense fallback={<div>Loading...</div>}>
+            <QuizWrapper type={"fill-in-the-blanks"} />
+          </Suspense>
         </div>
         {/* Right Column - Highlight */}
         <div className="w-full md:w-96 bg-gray-200 dark:bg-gray-800 p-8 shadow rounded">
-          <AnimatedQuizHighlight />
+          <Suspense fallback={<div>Loading...</div>}>
+            <AnimatedQuizHighlight />
+          </Suspense>
         </div>
       </div>
     </div>
