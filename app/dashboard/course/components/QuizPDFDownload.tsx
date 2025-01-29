fe579766fc229cb6e422cc23bf3095c0d4523841
santosh <santosh.ksingh03@gmail.com>
2025-01-29 20:28:13 +0800

    fix pdf

--- app/dashboard/course/components/QuizPDFDownload.tsx
+++ app/dashboard/course/components/QuizPDFDownload.tsx
@@ -1,17 +1,17 @@
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import dynamic from "next/dynamic";
-import QuizPDF, { QuizPDFProps } from "./QuizPDF";
+import ConfigurableQuizPDF, { QuizPDFProps } from "./QuizPDF";
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
 
 
 
-const QuizPDFDynamic = dynamic(() => Promise.resolve(QuizPDF), { ssr: false });
+const QuizPDFDynamic = dynamic(() => Promise.resolve(ConfigurableQuizPDF), { ssr: false });
 
 const QuizPDFDownload: React.FC<QuizPDFProps> = ({ quizData }) => {
   const quizSlug = "example-quiz"; // Replace with actual quiz slug
   return (
-    <PDFDownloadLink document={<QuizPDF quizData={quizData} />} fileName={`${quizSlug}-quiz.pdf`}>
+    <PDFDownloadLink document={<ConfigurableQuizPDF quizData={quizData} />} fileName={`${quizSlug}-quiz.pdf`}>
       {({ loading }: { loading: boolean }) => (
         <Button
           variant="outline"
