c5fd4f19fb921fb4f39a3c7a1bb226f8709bd3d7
santosh <santosh.ksingh03@gmail.com>
2025-01-31 12:59:27 +0800

    Loader Changes

--- app/components/GlobalLoader.tsx
+++ app/components/GlobalLoader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Watch } from "react-loader-spinner";
+import { ThreeCircles } from "react-loader-spinner";
 
 interface GlobalLoaderProps {
   loading: boolean;
@@ -9,14 +9,15 @@ export const GlobalLoader: React.FC<GlobalLoaderProps> = ({ loading }) => {
   if (loading) {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-gray-50 bg-opacity-75 z-50">
-        <Watch
-          visible={true}
-          height="80"
-          width="80"
-          radius="48"
-          color="#4fa94d"
-          ariaLabel="watch-loading"
-        />
+       <ThreeCircles
+  visible={true}
+  height="100"
+  width="100"
+  color="#4fa94d"
+  ariaLabel="three-circles-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  />
        
       </div>
     );
