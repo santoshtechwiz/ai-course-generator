047d165d626a9df955f71be6ce8ab37e4eb2ddba
santosh <santosh.ksingh03@gmail.com>
2025-02-16 15:28:04 +0800

    added admin board

---
+++ app/admin/components/data-table-pagination.tsx
@@ -0,0 +1,20 @@
+// "use client"
+
+// import type { Table } from "@tanstack/react-table"
+
+
+// interface DataTablePaginationProps<TData> {
+//   table: Table<TData>
+// }
+
+// export function DataTablePagination<TData>({ table }: DataTablePaginationProps<TData>) {
+//   return (
+//     <Pagination
+//       onPageChange={(page) => table.setPageIndex(page)}
+//       currentPage={table.getState().pagination.pageIndex + 1}
+//       pageSize={table.getState().pagination.pageSize}
+//       totalCount={table.getRowModel().rows.length}
+//     />
+//   )
+// }
+
