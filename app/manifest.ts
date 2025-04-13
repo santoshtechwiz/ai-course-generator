6c8dbca41b2c5e3e01590d947420e764006e28e7
santosh <santosh.ksingh03@gmail.com>
2025-04-13 22:06:14 +0800

    Fix Auth

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,22 +10,12 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#fff",
     theme_color: "#fff",
-    icons: [
+    "icons": [
       {
-        src: "/favicon.ico",
-        sizes: "48x48",
-        type: "image/x-icon",
-      },
-      {
-        src: "/logo192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/logo512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
+        "src": "/favicon.ico",
+        "sizes": "64x64 32x32 24x24 16x16",
+        "type": "image/x-icon"
+      }
     ],
     categories: ["education", "technology", "programming", "development"],
     related_applications: [
