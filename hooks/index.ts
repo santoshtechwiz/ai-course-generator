82884a99363a650bd5d361dbae089a340555be17
santosh <santosh.ksingh03@gmail.com>
2025-04-18 13:48:58 +0800

    fix

--- hooks/index.ts
+++ hooks/index.ts
@@ -4,17 +4,10 @@ export * from "./use-auth-check"
 export * from "./use-media-query"
 export * from "./use-quiz-result"
 export * from "./use-quiz-state"
+export * from "./useUserDashboard"
+export * from "./useCourseActions"
+export * from "./useChapterSummary"
+export * from "./useChapterProcessing"
+export * from "./use-toast"
 
 
-// Note: The following hooks appear to be unused or redundant and can be removed if not needed elsewhere:
-// - use-ripple.ts
-// - use-scroll-direction.tsx
-// - useChapterProcessing.ts
-// - useChapterSummary.ts
-// - useCourseActions.ts
-// - useInterval.ts
-// - usePersistentState.ts
-// - useProgress.ts
-// - useRandomQuizzes.ts
-// - useUserDashboard.ts
-// - useVideoState.ts
