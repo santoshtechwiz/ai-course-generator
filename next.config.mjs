5a77290df42325df08f2365d74f65b740374244a
santosh <santosh.ksingh03@gmail.com>
2025-06-22 22:11:36 +0800

    ddd

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-
-
 const nextConfig = {
   reactStrictMode: false,
   distDir: ".next",
-  standalone: true, // Enable standalone mode for better performance
   poweredByHeader: false, // Remove X-Powered-By header for security
 
   // Optimize image loading
