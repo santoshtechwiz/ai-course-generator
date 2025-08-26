9f5649b11b91f25cb5f3e38729afcd5f66ce9a1c
santosh <santosh.ksingh03@gmail.com>
2025-08-26 20:51:25 +0800

    ld

--- app/page.tsx
+++ app/page.tsx
@@ -48,8 +48,7 @@ export default function HomePage() {
             "price": "0",
             "priceCurrency": "USD"
           }
-        }}
-      />
+        }} type={""}      />
     </ModuleLayout>
   )
 }
