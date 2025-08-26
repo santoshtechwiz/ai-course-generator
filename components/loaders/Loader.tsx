76b039b51d127c57eea143a3b627b47a7b9fa408
santosh <santosh.ksingh03@gmail.com>
2025-08-26 20:46:22 +0800

    fx

--- components/loaders/Loader.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import { PageLoading } from '@/components/ui/loading'
-export const Loader = PageLoading
-export const LoadingSpinner = PageLoading
-export const InlineSpinner = PageLoading
-export const GlobalLoader = PageLoading
-export default PageLoading
