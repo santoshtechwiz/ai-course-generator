72a9984a06ac013ae66ea7a666eeff4bdc57cdbf
santosh <santosh.ksingh03@gmail.com>
2025-08-29 14:14:22 +0800

    FX

--- components/loaders/index.ts
+++
@@ -1,8 +0,0 @@
-"use client"
-
-export { SuspenseGlobalFallback } from './SuspenseGlobalFallback'
-export { Loader } from './loader'
-export type { LoaderProps } from './loader'
-export { progressApi } from './progress-api'
-export type { ProgressAPI, ProgressOptions } from './progress-api'
-export { useProgress } from './use-progress'
